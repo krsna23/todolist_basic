3c44f8fdc0f913f632f7eea848ec3d91b631de75
krsna23 <85450948+krsna23@users.noreply.github.com>
2024-01-22 15:00:02 +0530

    Javascript added and project completed
    Simple HTML CSS and Javascript project of Todolist is completed.

--- script.js
+++ script.js
@@ -10,5 +10,19 @@ function addTask(){
         li.setAttribute('class', 'task');
         li.innerHTML= inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML="\u00d7";
+        span.setAttribute('class', 'span');
+        li.appendChild(span);
     }
+    inputBox.value='';
 }
+
+listContainer.addEventListener('click', function(e){
+    if (e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
